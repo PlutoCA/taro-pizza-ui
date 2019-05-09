46c89841fc8fa618418ebe10653807c93e1182d3
xiamu <544652471@qq.com>
2019-05-09 21:30:41 +0800

    feat: change server port.

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 const init = async () => {
   const server = Hapi.server({
-    port: 3000,
+    port: process.env.PORT || 5000,
     host: "localhost",
     routes: {
       files: {
