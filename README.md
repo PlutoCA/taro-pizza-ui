99ef72cffa93b84d1effcd553b9e84aff303dd38
xiamu <544652471@qq.com>
2019-05-09 22:51:05 +0800

    feat: add readme.md/

---
+++ README.md
@@ -0,0 +1,10 @@
+# 介绍
+
+----
+
+`Taro Pizza UI` 是一款基于 [Taro](https://taro.aotu.io) 框架开发的多端 UI 组件库
+
+
+## 文档
+
+[Taro Pizza UI](https://taro-pizza.herokuapp.com/)
--- util/copy_build.js
+++ util/copy_build.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const toolkit = require("./toolkit");
 
+// NOTE: heroku will ignore build directory, so wo use $build
 toolkit.copyDir(
   path.join(__dirname, "../doc/build"),
   path.join(__dirname, "../doc/$build"),
